15bb8f79f6076fe2b781e0621cb9532e52141421
Shaharia66 <gssaif.tm@gmail.com>
2025-11-22 13:14:06 +0600

    day one for php

---
+++ example.php
@@ -0,0 +1,8 @@
+<?php
+echo 10+10 ;
+echo "<br/>";
+echo 'hellow World <br/> ';
+echo "asvksknfvjk <br/>";
+echo ("also work as a function.")
+
+?>               
---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+$name="taif";
+
+echo "saif";
+echo "<br/>";
+echo "saif";
+echo "<br/>";
+echo "saif";
+echo "<br/>";
+echo "saif";
+echo "<br/>";
+echo "saif";
+echo "<br/>";
+echo $name;
+echo "<br/>";
+echo $name;
+echo "<br/>";
+echo $name;
+echo "<br/>";
+?>
